2b127d263d237d1180d1fc8b579ee239c4995b2a
RamonSanGabriel <jeshero11@gmail.com>
2024-03-28 12:59:39 +0800

    task 2 complete

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -5,5 +5,4 @@ console.log(`Number of categories: ${categories.length}`);
 for (let category of categories) {
   console.log(`Category: ${category.firstElementChild.innerText}`);
   console.log(`Elements: ${category.firstElementChild.nextElementSibling.children.length}`);
-
 }
