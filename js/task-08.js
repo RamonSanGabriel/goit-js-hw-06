85cc90544ac38b4cd4dd5c337e33dfb67bc0ec85
RamonSanGabriel <jeshero11@gmail.com>
2024-04-02 00:58:04 +0800

    submit form

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector("form.login-form");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const email = document.querySelector("[name=email]").value;
+  const password = document.querySelector("[name=password]").value;
+
+  const user = {
+    email: "ramon123@yahoo.com",
+    password: "pass123",
+  };
+  if (email === user.email && password === user.password) {
+    alert("Login Successful!");
+  } else if (email === "" || password === "") {
+    alert("All fields must be filled in.");
+  } else {
+    alert("Invalid Credentials");
+  }
+  form.reset();
+});
