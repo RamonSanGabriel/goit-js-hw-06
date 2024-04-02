be30bb66f1c8dd0510b208ae6b3c256dd0aca932
RamonSanGabriel <jeshero11@gmail.com>
2024-04-02 21:16:43 +0800

    change background color on click button

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,21 @@
+// Get the elements we'll be working with
+const colorSpan = document.querySelector("span.color");
+const changeColorButton = document.querySelector("button.change-color");
+const body = document.querySelector("body");
+
+// Generate a random color
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+// Change the background color and update the displayed color value
+const changeBackgroundColor = () => {
+  const newColor = getRandomHexColor(colorSpan);
+  body.style.backgroundColor = newColor;
+  colorSpan.textContent = newColor;
+};
+
+// Attach the event listener to the button
+changeColorButton.addEventListener("click", changeBackgroundColor);
